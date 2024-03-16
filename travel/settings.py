264ab4727e81b10accddd376943ac71db4fadb48
Sato.Saiko <3221217388@qq.com>
2024-03-16 16:32:38 +0800

    链接本地数据库，完善登录逻辑

--- travel/settings.py
+++ travel/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'travel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database_test_0',
+        'USER': 'django_',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
